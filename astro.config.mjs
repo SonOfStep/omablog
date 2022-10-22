d72f4fc0eee392a61dbf233af87cc8b9276030de
Омар Нурмаханов <omarionurm@gmail.com>
2022-10-22 21:09:55 +0500

    Add mode switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,10 +13,13 @@ import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx()],
+  }), mdx(), react()],
   output: "server",
   adapter: netlify()
 });
