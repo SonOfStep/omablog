3c10e67eaefe415098f0a139b3f0d8f27c8a9073
Омар Нурмаханов <omarionurm@gmail.com>
2022-11-03 02:10:28 +0500

    decomposition and fix dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,31 +3,33 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-      extend: {
-        colors: {
-          background: "#16161A",
-          paragraph: "#94A1B2",
-          headline: "#FFFFFE",
-          button: "#7F5AF0",
-          ["button-text"]: "#FFFFFE",
-          ["background-secondary"]: "#242629",
-        },
-        fontFamily: {
-          roboto: [
-            "Roboto",
-            "Segoe UI",
-            "Tahoma",
-            "Geneva",
-            "Verdana",
-            "sans-serif",
-          ],
+    extend: {
+      colors: {
+        background: "#16161A",
+        paragraph: "#94A1B2",
+        headline: "#FFFFFE",
+        button: "#7F5AF0",
+        ["button-text"]: "#FFFFFE",
+        ["background-secondary"]: "#242629",
+        light: {
+          background: "#FFFFFE",
+          paragraph: "#2B2C34",
+          headline: "#2B2C34",
+          button: "#6246EA",
+          ["background-secondary"]: "#D1D1E9",
         },
       },
+      fontFamily: {
+        roboto: [
+          "Roboto",
+          "Segoe UI",
+          "Tahoma",
+          "Geneva",
+          "Verdana",
+          "sans-serif",
+        ],
+      },
     },
-    plugins: [],
   },
+  plugins: [],
 };
