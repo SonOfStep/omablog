e8231b93c41cc16e8ba925661113e0f8683d45da
Омар Нурмаханов <omarionurm@gmail.com>
2022-11-02 19:43:21 +0500

    restart

--- src/config.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = "OmaBlog. Мой персональный сайт";
-export const SITE_DESCRIPTION = "Добро пожаловать на мой личный сайт";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+=======
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#16161A",
+        paragraph: "#94A1B2",
+        headline: "#FFFFFE",
+        button: "#7F5AF0",
+        ["button-text"]: "#FFFFFE",
+        ["background-secondary"]: "#242629",
+      },
+      fontFamily: {
+        roboto: [
+          "Roboto",
+          "Segoe UI",
+          "Tahoma",
+          "Geneva",
+          "Verdana",
+          "sans-serif",
+        ],
+      },
+    },
+>>>>>>> 1acbdbe (restart)
   },
   plugins: [],
 };
